7308243b03fb1beee8352fa6b5a965c769fd3103
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-09-25 00:19:01 -0400

    added time period data aggregation with DF

--- MVP/spark_s3_csv_to_df.py
+++ MVP/spark_s3_csv_to_df.py
@@ -5,30 +5,51 @@ from pyspark.sql.functions import col, substring, unix_timestamp, concat, lit
 
 conf = SparkConf().setAppName("read_csv").setMaster("local")
 
+# define data location on S3 ###################################################
 region = 'us-east-2'
 bucket = 'maxwell-insight'
-key = '2019-Nov.csv'
+#key = '2019-Nov.csv'
+key = 'sample.csv'
 s3file = f's3a://{bucket}/{key}'
+################################################################################
 
+# initialize spark session and spark context####################################
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc)
 sql_c = SQLContext(sc)
+################################################################################
 
+# other settings ###############################################################
+# timestep of specific item data to group by, in seconds, for plotting
+tstep = 60
+# start time for time series plotting, I'll set this to a specific time for now
+t0 = unix_timestamp("2019-11-01 00:00:00 UTC", 'yyyy-MM-dd HH:mm:ss z')
+################################################################################
+
+# read csv file on s3 into spark dataframe
 df = sql_c.read.csv(s3file, header=True)
+
+# drop unused column
 df = df.drop('_c0')
 
+# convert data and time into timestamps, remove orginal date time column
+# reorder column so that timestamp is leftmost
 df = df.withColumn(
-        'date_time', substring('event_time',0,19))
+    'timestamp', unix_timestamp(col("event_time"), 'yyyy-MM-dd HH:mm:ss z')
+    ).select(['timestamp']+df.columns[:-1]).drop('event_time')
 
-#df.show(n=50,truncate=False)
+# create new column consisting of product_id + "-" + number of timesteps from
+# start time t0. Rows belonging to same product_id and within same chunck of timestep
+# will be aggregated together.
+# Eg. Views of product_id 10000000001 between 00:00:00 to 00:01:00 will have new column
+# value to be 10000000001-000000
 
-new_df = df.withColumn(
-    'timestamp', unix_timestamp(col("date_time"), 'yyyy-MM-dd HH:mm:ss')
-    ).select(['timestamp']+df.columns[:-1]).drop('date_time').drop('event_time')
+df = df.withColumn('pid_timeperiod',
+[concat(col("product_id"), lit("-"), lpad(df.timestamp - t0,10,0))])
 
-new_df = new_df.select([concat(col("product_id"), lit("-"), col("timestamp"))] + new_df.columns )
-new_df = new_df.withColumnRenamed(new_df.columns[0], "pid_timestamp")
+#df = df.select([concat(col("product_id"), lit("-"), col("timestamp"))] + df.columns )
+# df2 = df2.withColumnRenamed(df2.columns[0], "pid_timestamp")
 
-new_df = new_df.sort("pid_timestamp")
+df2 = df2.sort("pid_timeperiod")
 
 new_df.show(n=100, truncate=False)
