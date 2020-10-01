1e14d9914551f60db49a6689abd2bc70d2317a39
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-01 02:20:36 -0400

    dash psql test

--- tests/dash_test/dash_connect_psql.py
+++ tests/dash_test/dash_connect_psql.py
@@ -24,8 +24,10 @@ from sqlalchemy import create_engine
 # Import data from postgreSQL using sqlalchemy
 engine = create_engine(f"postgresql://{os.environ['psql_username']}:{os.environ['psql_pw']}@localhost:5431/my_db")
 print (engine)
-df = pd.read_sql_table("event_count", engine).sort_values(by=["view_cnt"], ascending=False)
-print (df.head(50))
+df = pd.read_sql_table("event_count", engine)
+df1 = df.groupby("product_id").sort_values(by=["view_cnt"], ascending=False)
+
+print (df1.head(50))
 
 # # dash Application
 # app = dash.Dash(__name__)
