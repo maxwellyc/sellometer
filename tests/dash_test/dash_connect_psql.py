543957b059850d2643289c198eeff204f696644a
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-01 02:39:19 -0400

    dash psql test

--- tests/dash_test/dash_connect_psql.py
+++ tests/dash_test/dash_connect_psql.py
@@ -33,7 +33,7 @@ g1 = df1.groupby(by="product_id").sum()#.sort_values(by="view_cnt")
 #df1.sort_values(by=["view_cnt"], ascending=False)
 g1.add_suffix('_Count').reset_index()
 g1 = g1.sort_values(by="view_cnt", ascending=False)
-print (g1.head(20))
+print (list(g1.head(20)['product_id']))
 
 # # dash Application
 # app = dash.Dash(__name__)
