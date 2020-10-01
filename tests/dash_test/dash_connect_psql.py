56c0ca11b0bf3bb64a652ede24644768dbe2111d
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-01 03:22:55 -0400

    dash psql test

--- tests/dash_test/dash_connect_psql.py
+++ tests/dash_test/dash_connect_psql.py
@@ -23,30 +23,27 @@ from sqlalchemy import create_engine
 
 # Import data from postgreSQL using sqlalchemy
 engine = create_engine(f"postgresql://{os.environ['psql_username']}:{os.environ['psql_pw']}@localhost:5431/my_db")
-print (engine)
+
 df = pd.read_sql_table("event_count", engine)
-print (df.head(20))
 
 df1 = df.copy()
 g1 = df1.groupby(by="product_id").sum()#.sort_values(by="view_cnt")
 #gb = df.groupby(by="product_id").sum()#.apply(lambda _df: _df.sort_values('view_cnt'))
-#df1.sort_values(by=["view_cnt"], ascending=False)
+
 g1.add_suffix('_Count').reset_index()
 g1 = g1.sort_values(by="view_cnt", ascending=False)
 
-top_20_id = list(g1.index.get_level_values(0))[:10]
+top_ids = list(g1.index.get_level_values(0))[:10]
 
 views_ts = {}
-for id in top_20_id:
+for id in top_ids:
     views_ts[id] = [list((df.loc[df['product_id'] == id])['time_period']),
     list((df.loc[df['product_id'] == id])['view_cnt'])]
-    print (views_ts[id])
 
 dropdown_op = []
 
 for id in views_ts:
     dropdown_op.append({"label":str(id), "value": id})
-
 
 dff = df.copy()
 dff = dff[dff["product_id"] == min(views_ts.keys())]
