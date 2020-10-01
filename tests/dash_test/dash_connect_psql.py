b634f1385b2d99a73a3e9b8b49affc98194ef7ad
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-01 01:23:33 -0400

    dash psql test

--- tests/dash_test/dash_connect_psql.py
+++ tests/dash_test/dash_connect_psql.py
@@ -15,7 +15,7 @@ app = dash.Dash(__name__)
 
 # ------------------------------------------------------------------------------
 # Import data from postgreSQL database
-conn = psycopg2.connect(dbname="event_count", user=os.environ['psql_username'],\
+conn = psycopg2.connect(dbname="my_db", user=os.environ['psql_username'],\
  password=os.environ['psql_pw'], host="localhost", port=5431) # connection string
 
 print (conn)
