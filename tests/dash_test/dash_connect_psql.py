b9bac94f0d357d7a14afd3da4ec77041e74105cc
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-01 03:29:12 -0400

    dash psql test

--- tests/dash_test/dash_connect_psql.py
+++ tests/dash_test/dash_connect_psql.py
@@ -51,7 +51,7 @@ print (dff)
 s = pd.to_numeric(dff['time_period'])
 print (s)
 print (dff)
-dff = dff.merge(s.rename("time_period"), left_index=True, right_index=True)
+dff = dff.merge(s.to_frame(), left_index=True, right_index=True)
 print (dff)
 
 # # dash Application
