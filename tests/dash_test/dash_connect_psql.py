2b8aad2a48ea30d6a3d4aeb0d46f1901d516624c
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-01 03:32:35 -0400

    dash psql test

--- tests/dash_test/dash_connect_psql.py
+++ tests/dash_test/dash_connect_psql.py
@@ -50,7 +50,7 @@ dff = dff[dff["product_id"] == min(views_ts.keys())]
 print (dff)
 s = pd.to_numeric(dff['time_period'])
 print (dff.columns)
-dff.drop(axis='time_period')
+dff.drop(['time_period'])
 dff = dff.merge(s.to_frame(), left_index=True, right_index=True)
 print (dff)
 
