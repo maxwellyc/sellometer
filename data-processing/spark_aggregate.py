9037cc88dc17690fdc0d6568f884c3579b6dbd3e
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-06 03:59:51 -0400

    addtional aggregation

--- data-processing/spark_aggregate.py
+++ data-processing/spark_aggregate.py
@@ -129,7 +129,7 @@ def main():
                                 .agg(F.sum('price')))
 
     purchase_dims['product_id'].show()
-    purchase_dims['brand'].show()
+    purchase_dims['brand'].orderBy('brand').show()
 
     # write dataframe to postgreSQL
     #
