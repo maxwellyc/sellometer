a674b3f6fe25947604c002ede4969294695db9a4
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-06 03:48:51 -0400

    addtional aggregation

--- data-processing/spark_aggregate.py
+++ data-processing/spark_aggregate.py
@@ -116,10 +116,10 @@ def main():
     # total view counts per dimesion, total sales amount per dimension
     for dim in dimensions:
         # total view counts per dimension, if product_id, also compute mean price
-        view_dims[dim] = view_df.groupby(by=[dim, 'event_time']).agg(
+        view_dims[dim] = view_df.groupby(dim, 'event_time').agg(
         ['count'] if dim != 'product_id' else ['count','mean'])['price']
         # total $$$ amount sold per dimension, if product_id also compute count and mean
-        purchase_dims[dim] = purchase_df.groupby(by=[dim, 'time_period']).agg(
+        purchase_dims[dim] = purchase_df.groupby(dim, 'time_period').agg(
         ['sum'] if dim != 'product_id' else ['sum','count','mean'])['price']
 
     purchase_dims['product_id'].show()
