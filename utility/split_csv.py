820aa81bcf146b7a75f5a499e27da2f1ddef3a33
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-07 03:46:09 -0400

    split csv update

--- utility/split_csv.py
+++ utility/split_csv.py
@@ -36,7 +36,7 @@ def main():
         for i in range(6):
             f_name = t0.strftime("%Y-%m-%d-%H-%M-%S") + '-' + str(i) + '.csv'
             df_i = df_temp.iloc[i::6, :]
-            df_i.to_csv("s3://maxwell-insight/minicsv/" + f_name)
+            df_i.to_csv("s3://ecommerce-mini/" + f_name)
         t0 += pd.Timedelta(seconds=60)
         t1  = t0 + pd.Timedelta(seconds=60)
 
