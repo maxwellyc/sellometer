12d7b81d72b133739ef82fc2f83a80d1407720e9
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-07 01:47:10 -0400

    split csv test

--- utility/split_csv.py
+++ utility/split_csv.py
@@ -18,15 +18,13 @@ def main():
     ################################################################################
 
     # compress time, 60 second -> 1 second
-    t_step = 60
+    t_step = 60 # unit in seconds, timestamp will be grouped in steps with stepsize of t_step seconds
+    df['event_time'] = ((pd.to_datetime(df['event_time']) - pd.Timestamp("1970-01-01") )  / pd.Timedelta('1s'))
     t_min = df['event_time'].min()
-    df['event_time'] = pd.to_datetime(df['event_time'])
-    # df['event_time'] = df['event_time'].values.astype(int)  // 10 ** 9
-    # df['event_time'] = df['event_time'] - t_min
-    # df['event_time'] = df['event_time'] // t_step
-    # df['event_time'] = df['event_time'] + t_min
-    # df['event_time'] = pd.to_datetime(df['event_time'],unit='s', origin='unix')
-
+    df['event_time'] = df['event_time'] - t_min
+    df['event_time'] = df['event_time'] // t_step
+    df['event_time'] = (df['event_time'] + t_min).astype(np.int64)
+    df['event_time'] = pd.to_datetime(df['event_time'], unit='s', origin='unix')
     print (df.head(100))
 
 
