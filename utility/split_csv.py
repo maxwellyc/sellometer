29ba731b319645e684359b2cf42f165e6121f34f
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-07 02:37:14 -0400

    split csv test

--- utility/split_csv.py
+++ utility/split_csv.py
@@ -24,8 +24,20 @@ def main():
     df['event_time'] = df['event_time'] - t_min
     df['event_time'] = df['event_time'] // t_step
     df['event_time'] = (df['event_time'] + t_min).astype(np.int64)
-    df['event_time'] = pd.to_datetime(df['event_time'], unit='s').dt.strftime("%Y-%m-%d %H:%M:%S")
-    print (df.head(500))
+    df['event_time'] = pd.to_datetime(df['event_time'], unit='s')#.dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    t0 = df['event_time'].min()
+    t1 = t0 + pd.Timedelta(seconds=60)
+    t_end = df['event_time'].max()
+
+    while t0 < t_end:
+        df_temp = df[t0 : t1]
+        for i in range(6):
+            f_name = t0.strftime("%Y-%m-%d %H:%M") + '-' + str(i) + '.csv'
+            df_i = df_temp.iloc[i::6, :]
+            df_i.to_csv("mini_csv/" + f_name)
+        t0 += pd.Timedelta(seconds=60)
+        t1  = t0 + pd.Timedelta(seconds=60)
 
 
 if __name__ == "__main__":
