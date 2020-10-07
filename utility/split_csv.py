1d2a8313d525f72da6b42fcb2cc5b9e9a75a308e
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-07 13:15:20 -0400

    csv split test

--- utility/split_csv.py
+++ utility/split_csv.py
@@ -4,24 +4,25 @@ import numpy as np
 from io import StringIO # python3; python2: BytesIO
 import boto3
 
-def main(key = '2019-Oct'):
+def main(key = '2020-Feb'):
     key = key + ".csv"
+    key = 'sample.csv'
     ################################################################################
-
-    # read data from S3 ############################################################
-    # for mini batches need to change this section into dynamical
+    # read csv file on s3 into spark dataframe
+    df = pd.read_csv(key)
     region = 'us-east-2'
     bucket = 'maxwell-insight'
-    key = 'sample.csv'
+    # key = 'sample.csv'
     s3file = f"s3a://{bucket}/{key}"
     # read csv file on s3 into spark dataframe
     df = pd.read_csv(s3file)
+
     ################################################################################
 
     # compress time, 60 second -> 1 second
-    t_step = 60 # unit in seconds, timestamp will be grouped in steps with stepsize of t_step seconds
+    t_step = 30 # unit in seconds, timestamp will be grouped in steps with stepsize of t_step seconds
     df['event_time'] = ((pd.to_datetime(df['event_time']) - pd.Timestamp("1970-01-01", tz='utc') )  / pd.Timedelta('1s'))
-    t_min = df['event_time'].min()
+    t_min = pd.Timestamp("2019-10-01", tz='utc')
     df['event_time'] = df['event_time'] - t_min
     df['event_time'] = df['event_time'] // t_step
     df['event_time'] = (df['event_time'] + t_min).astype(np.int64)
@@ -33,9 +34,10 @@ def main(key = '2019-Oct'):
 
     while t0 < t_end:
         df_temp = df[ (df['event_time'] > t0) & (df['event_time'] < t1 )]
-        for i in range(6):
+        for i in range(4):
             f_name = t0.strftime("%Y-%m-%d-%H-%M-%S") + '-' + str(i) + '.csv'
-            df_i = df_temp.iloc[i::6, :]
+            print (f_name)
+            df_i = df_temp.iloc[i::4, :]
             csv_buffer = StringIO()
             df_i.to_csv(csv_buffer)
             s3_resource = boto3.resource('s3')
@@ -45,5 +47,5 @@ def main(key = '2019-Oct'):
 
 
 if __name__ == "__main__":
-    for key in ["2019-Oct","2019-Nov","2019-Dec","2020-Jan","2020-Feb","2020-Mar","2020-Apr"]:
-        main(key)
+    #for key in ["2019-Oct","2019-Nov","2019-Dec","2020-Jan","2020-Feb","2020-Mar","2020-Apr"]:
+    main(key = "2019-Dec")
