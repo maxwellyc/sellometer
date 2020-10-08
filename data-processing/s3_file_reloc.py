8e8cb294b8f111ddea49465ee34f6ffecad3baff
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-07 23:25:25 -0400

    s3 file relocation using boto3

--- data-processing/s3_file_reloc.py
+++ data-processing/s3_file_reloc.py
@@ -1,12 +1,13 @@
 import boto3
 s3 = boto3.resource('s3')
 
+bucket = 'maxwell-insight'
+src_dir = 'serverpool/'
+dst_dir = 'spark-processed/'
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+result = client.list_objects(Bucket=bucket, Prefix=prefix, Delimiter='/')
+for k in result:
+    print (k)
 
-buckets = s3.Bucket('maxwell-insight')
-# dst = s3.Bucket('maxwell-insight/spark-processed/')
-
-for file in buckets.list('serverpool'):
-    print(file)
+# s3_resource.Object('maxwell-insight', "").copy_from(
+#  CopySource=”path/to/your/object_A.txt”)
