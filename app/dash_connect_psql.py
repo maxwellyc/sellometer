e884bab8b90c8bcb1e95d8039ebbe91fb55fc4c1
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-09 05:21:17 -0400

    dash app modify

--- app/dash_connect_psql.py
+++ app/dash_connect_psql.py
@@ -17,9 +17,9 @@ def read_sql_to_df(engine, table_name="purchase_product_id_hour", id_name = 'pro
     return df, df_gb
 
 def rank_by_id(df_gb, rank_metric = "count(price)", n = 10):
-    df = df.sort_values(by=rank_metric, ascending=False)
-    hot_id_list = list(df.index.get_level_values(0))[:n]
-    hot_list = [ (id, df.loc[id, rank_metric]) for id in hot_id_list]
+    df_gb = df_gb.sort_values(by=rank_metric, ascending=False)
+    hot_id_list = list(df_gb.index.get_level_values(0))[:n]
+    hot_list = [ (id, df_gb.loc[id, rank_metric]) for id in hot_id_list]
     return hot_list
 
 def id_time_series(hot_list, df, id_name = 'product_id'):
