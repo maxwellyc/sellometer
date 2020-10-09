c56d4ac5e5275faa52372d45911c4fb91677cea5
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-09 05:02:38 -0400

    dash app modify

--- app/dash_connect_psql.py
+++ app/dash_connect_psql.py
@@ -11,104 +11,95 @@ import psycopg2
 import os
 from sqlalchemy import create_engine
 
-# # ------------------------------------------------------------------------------
-# # Import data from postgreSQL using psycopg2
-# conn = psycopg2.connect(dbname="my_db", user=os.environ['psql_username'],\
-#  password=os.environ['psql_pw'], host="localhost", port=5431) # connection string
-# cur = conn.cursor() # cursor object
+def read_sql_to_gb(engine, table_name="purchase_product_id_hour", id_name = 'product_id'):
+    df = pd.read_sql_table(table_name, engine)
+    gb = df.groupby(by=id_name).sum()
+    gb.show(50)
+    return gb
+
+def rank_by_id(gb, rank_metric = "count(price)", n = 10):
+    gb = gb.sort_values(by=rank_metric, ascending=False)
+    hot_id_list = list(gb.index.get_level_values(0))[:n]
+    hot_list = [ [hot_id_list, gb[ hot_id_list ], rank_metric]] for id in hot_id_list]
+    return hot_list
+
+def id_time_series(hot_list, df, id_name = 'product_id'):
+    time_series_by_id = {}
+    for id in hot_list:
+        time_series_by_id[id] = [list((df.loc[df[id_name] == id])['event_time']),
+        list((df.loc[df[id_name] == id])['sum(price)'])]
+
+    dropdown_op = []
+
+    for id in hot_list:
+        dropdown_op.append({"label":str(id), "value": id})
+
+    # dff = df.copy()
+    # dff = dff[dff["product_id"] == min(views_ts.keys())]
+    # s = pd.to_numeric(dff['time_period'])
+    # dff = dff.drop(columns=['time_period'])
+    # dff = dff.merge(s.to_frame(), left_index=True, right_index=True)
+    # print (dff)
+
+# # dash Application
+# app = dash.Dash(__name__)
 #
-# cur.execute('''SELECT * FROM event_count''')
-# for row in cur.fetchall():
-#     print (row)
-
-# Import data from postgreSQL using sqlalchemy
-engine = create_engine(f"postgresql://{os.environ['psql_username']}:{os.environ['psql_pw']}@10.0.0.5:5431/ecommerce")
-
-df = pd.read_sql_table("purchase_product_id", engine)
-
-df1 = df.copy()
-g1 = df1.groupby(by="product_id").sum()#.sort_values(by="view_cnt")
-#gb = df.groupby(by="product_id").sum()#.apply(lambda _df: _df.sort_values('view_cnt'))
-
-g1.add_suffix('_Count').reset_index()
-g1 = g1.sort_values(by="count(price)", ascending=False)
-
-top_ids = list(g1.index.get_level_values(0))[:10]
-
-views_ts = {}
-for id in top_ids:
-    views_ts[id] = [list((df.loc[df['product_id'] == id])['time_period']),
-    list((df.loc[df['product_id'] == id])['sum(price)'])]
-
-dropdown_op = []
-
-for id in views_ts:
-    dropdown_op.append({"label":str(id), "value": id})
-
-dff = df.copy()
-dff = dff[dff["product_id"] == min(views_ts.keys())]
-s = pd.to_numeric(dff['time_period'])
-dff = dff.drop(columns=['time_period'])
-dff = dff.merge(s.to_frame(), left_index=True, right_index=True)
-print (dff)
-
-# dash Application
-app = dash.Dash(__name__)
-
-# ------------------------------------------------------------------------------
-# App layout
-app.layout = html.Div([
-
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
-
-    dcc.Dropdown(id="slct_item",
-                 options=dropdown_op,
-                 multi=False,
-                 value=min(views_ts.keys()),
-                 style={'width': "40%"}
-                 ),
-
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-
-    dcc.Graph(id='views_timeseries', figure={})
-
-])
-
-
-# ------------------------------------------------------------------------------
-# Connect the Plotly graphs with Dash Components
-@app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='views_timeseries', component_property='figure')],
-    [Input(component_id='slct_item', component_property='value')]
-)
-def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
-
-    container = "The item id chosen by user was: {}".format(option_slctd)
-
-    dff = df.copy()
-    dff = dff[dff["product_id"] == option_slctd]
-    s = pd.to_numeric(dff['time_period'])
-    dff = dff.drop(columns=['time_period'])
-    dff = dff.merge(s.to_frame(), left_index=True, right_index=True)
-
-    # Plotly Express
-    fig = px.scatter(
-        data_frame=dff,
-        x = 'time_period',
-        y = 'sum(price)',
-        color = 'product_id',
-        labels={'sum(price)': 'GMV ($)',
-        'time_period':'time since start (minutes)'},
-        template='plotly_dark'
-    )
-
-    return container, fig
+# # ------------------------------------------------------------------------------
+# # App layout
+# app.layout = html.Div([
+#
+#     html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+#
+#     dcc.Dropdown(id="slct_item",
+#                  options=dropdown_op,
+#                  multi=False,
+#                  value=min(views_ts.keys()),
+#                  style={'width': "40%"}
+#                  ),
+#
+#     html.Div(id='output_container', children=[]),
+#     html.Br(),
+#
+#     dcc.Graph(id='views_timeseries', figure={})
+#
+# ])
+#
+#
+# # ------------------------------------------------------------------------------
+# # Connect the Plotly graphs with Dash Components
+# @app.callback(
+#     [Output(component_id='output_container', component_property='children'),
+#      Output(component_id='views_timeseries', component_property='figure')],
+#     [Input(component_id='slct_item', component_property='value')]
+# )
+# def update_graph(option_slctd):
+#     print(option_slctd)
+#     print(type(option_slctd))
+#
+#     container = "The item id chosen by user was: {}".format(option_slctd)
+#
+#     dff = df.copy()
+#     dff = dff[dff["product_id"] == option_slctd]
+#     s = pd.to_numeric(dff['time_period'])
+#     dff = dff.drop(columns=['time_period'])
+#     dff = dff.merge(s.to_frame(), left_index=True, right_index=True)
+#
+#     # Plotly Express
+#     fig = px.scatter(
+#         data_frame=dff,
+#         x = 'time_period',
+#         y = 'sum(price)',
+#         color = 'product_id',
+#         labels={'sum(price)': 'GMV ($)',
+#         'time_period':'time since start (minutes)'},
+#         template='plotly_dark'
+#     )
+#
+#     return container, fig
 
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051, host="10.0.0.12")
+    engine = create_engine(f"postgresql://{os.environ['psql_username']}:{os.environ['psql_pw']}@10.0.0.5:5431/ecommerce")
+    gb = read_sql_to_df(engine, table_name="purchase_product_id_hour", id_name = 'product_id')
+    # app.run_server(debug=True, port=8051, host="10.0.0.12")
