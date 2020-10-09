f48c48ce6745258947e83b27f85d23db81a47d75
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-09 08:18:12 -0400

    dash update

--- app/dash_live.py
+++ app/dash_live.py
@@ -42,10 +42,9 @@ def update_df():
     df_hour, df_gb_hour = read_sql_to_df(engine, table_name="purchase_product_id_minute", id_name = 'product_id')
     hot_list = rank_by_id(df_gb_hour, rank_metric = "count(price)", n = 10)
     df_by_id, dropdown_op = id_time_series(hot_list, df, id_name = 'product_id')
-    df_gb_hour = df_gb_hour[df_gb_hour['product_id'].isin([id for id, m in hot_list])].astype({"product_id":str})
-    return df_by_id, hot_list, df_gb_hour
+    return df_by_id, hot_list
 
-df_by_id, hot_list, df_gb_hour = update_df()
+df_by_id, hot_list = update_df()
 # # dash Application
 app = dash.Dash(__name__)
 
@@ -79,8 +78,10 @@ app.layout = html.Div([
               ]
 )
 def update_graph_scatter(n, p_id):
-    df_by_id, hot_list, df_gb_hour = update_df()
-
+    df_by_id, hot_list = update_df()
+    hot_list.sort(key=lambda x: x[1])
+    data = {'Product-ID' : [str(id) for id, m in hot_list], "Quantity-Sold":[m for id, m in hot_list] }
+    df = pd.DataFrame.from_dict(data)
     #print (df.head(50))
     plot_df = df_by_id[p_id]
     # Plotly Go
@@ -94,9 +95,9 @@ def update_graph_scatter(n, p_id):
     )
 
     barchart = px.bar(
-        data_frame = df_gb_hour,
-        x = "product_id",
-        y = "count(price)",
+        data_frame = df,
+        x = 'Product-ID',
+        y = "Quantity-Sold",
         orientation = "h",
         barmode='relative',
         labels={"product_id":"Product Id",
