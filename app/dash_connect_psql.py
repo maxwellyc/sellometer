b8b69cc36dbac22ddb02d0d083b6d3d900ebf76d
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-09 05:39:17 -0400

    dash app modify

--- app/dash_connect_psql.py
+++ app/dash_connect_psql.py
@@ -31,6 +31,11 @@ def id_time_series(hot_list, df, id_name = 'product_id'):
         dropdown_op.append({"label":f"{id_name}: {id}", "value": id})
     return time_series_by_id, dropdown_op
 
+engine = create_engine(f"postgresql://{os.environ['psql_username']}:{os.environ['psql_pw']}@10.0.0.5:5431/ecommerce")
+df, df_gb = read_sql_to_df(engine, table_name="purchase_product_id_minute", id_name = 'product_id')
+hot_list = rank_by_id(df_gb, rank_metric = "count(price)", n = 10)
+time_series_by_id, dropdown_op = id_time_series(hot_list, df, id_name = 'product_id')
+
 # # dash Application
 app = dash.Dash(__name__)
 
@@ -90,8 +95,4 @@ def update_graph(option_slctd):
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    engine = create_engine(f"postgresql://{os.environ['psql_username']}:{os.environ['psql_pw']}@10.0.0.5:5431/ecommerce")
-    df, df_gb = read_sql_to_df(engine, table_name="purchase_product_id_minute", id_name = 'product_id')
-    hot_list = rank_by_id(df_gb, rank_metric = "count(price)", n = 10)
-    time_series_by_id, dropdown_op = id_time_series(hot_list, df, id_name = 'product_id')
     app.run_server(debug=True, port=8051, host="10.0.0.12")
