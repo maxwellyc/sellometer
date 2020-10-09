be74aeca8141895f21496ffc1a814cb97c363178
Ubuntu <ubuntu@ip-10-0-0-11.us-east-2.compute.internal>
2020-10-09 02:29:27 +0000

    mimic server script

--- utility/mimic_server.py
+++ utility/mimic_server.py
@@ -3,4 +3,6 @@ import os
 bucket = 'maxwell-insight'
 src_dir = 'serverpool/'
 dst_dir = 'spark-processed/'
-os.system(f's3cmd mv s3://{bucket}/{src_dir}* s3://{bucket}/{dst_dir}')
+
+#os.system(f's3cmd mv s3://{bucket}/{src_dir}*.csv s3://{bucket}/{dst_dir}')
+os.system(f's3cmd cp s3://{bucket}/{src_dir}*.csv s3://{bucket}/{dst_dir}')
