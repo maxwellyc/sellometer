d83cb2eb341e9244a05e6b17296a467dd9d3c27b
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-14 03:02:02 -0400

    housekeep code debug

--- data-processing/housekeep_min_to_hour.py
+++ data-processing/housekeep_min_to_hour.py
@@ -145,10 +145,10 @@ def min_to_hour(dimensions, events):
             # read min data from t1 datatable
             df_0 = read_sql_to_df(spark,event=evt,dim=dim,suffix='minute')
             # remove data from more than 24 hours away from t1 datatable
-            df = remove_min_data_from_sql(df_0, curr_min, hours_window = 24)
+            df_cut = remove_min_data_from_sql(df_0, curr_min, hours_window = 24)
             df.show(50)
             # rewrite minute level data
-            write_to_psql(df, evt, dim, mode="overwrite", suffix='minute_test')
+            write_to_psql(df_cut, evt, dim, mode="overwrite", suffix='minute')
 
             # slice 3600 second of dataframe for ranking purpose
             # rank datatable is a dynamic sliding window and updates every minute
