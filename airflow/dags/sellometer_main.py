a875e6feed7a3e098cdb00754d6fb92506a7c9fe
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-15 03:11:12 -0400

    check backlog debug

--- airflow/dags/sellometer_main.py
+++ airflow/dags/sellometer_main.py
@@ -8,12 +8,11 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 import os, subprocess, sys, imp
 util = imp.load_source('util', '/home/ubuntu/eCommerce/data-processing/check_backlog.py')
-# sys.path.append('/home/ubuntu/eCommerce/data_processing')
-# sys.path.insert(0,os.path.abspath(os.path.dirname('/home/ubuntu/eCommerce/data-processing/')))
-#sys.path.insert(0,os.path.abspath(os.path.dirname('/home/ubuntu/eCommerce/data_processing')))
+
 bucket = 'maxwell-insight'
 dimensions = ['product_id', 'brand', 'category_l3'] #  'category_l1','category_l2'
 events = ['purchase', 'view'] # test purchase then test view
+
 args = {
     'owner': 'airflow',
     'retries': 1,
@@ -22,12 +21,14 @@ args = {
     'wait_for_downstream':False,
     'retry_delay': timedelta(seconds=5),
     }
+
 dag = DAG(
     dag_id='main_spark_process',
     schedule_interval=timedelta(seconds=60),
     max_active_runs=1,
     default_args=args
     )
+
 def run_streaming():
     response = subprocess.check_output(f's3cmd du $s3/serverpool/', shell=True).decode('ascii')
     file_size = float(response.split(" ")[0]) / 1024 / 1024 # total file size in Mbytes
@@ -36,14 +37,12 @@ def run_streaming():
     print(max_cores,'spark cores executing')
     os.system(f'spark-submit --conf spark.cores.max={max_cores} ' +\
     '$sparkf ~/eCommerce/data-processing/streaming.py')
+
 def run_backlog_processing():
-    response = subprocess.check_output(f's3cmd du $s3/backlogs/', shell=True).decode('ascii')
-    file_size = float(response.split(" ")[0]) / 1024 / 1024 # total file size in Mbytes
-    # use extra processors when file size greater than 10 Mb
-    max_cores = 12 if file_size > 10 else 10
-    print(max_cores,'spark cores executing')
-    os.system(f'spark-submit --conf spark.cores.max={max_cores} ' +\
+    os.system(f'spark-submit --conf spark.cores.max=12 ' +\
     '$sparkf ~/eCommerce/data-processing/backlog_processing.py')
+
+
 new_file_sensor = S3KeySensor(
     task_id='new_csv_sensor',
     poke_interval= 3, # (seconds); checking file every 4 seconds
@@ -52,36 +51,26 @@ new_file_sensor = S3KeySensor(
     bucket_name=None,
     wildcard_match=True,
     dag=dag)
+
 spark_live_process = PythonOperator(
   task_id='spark_live_process',
   python_callable=run_streaming,
   trigger_rule='none_failed',
-  dag = dag
-  )
+  dag = dag)
+
 check_backlog = BranchPythonOperator(
     task_id='check_backlog',
     python_callable=util.collect_backlogs,
     dag = dag)
+
 process_backlogs = PythonOperator(
     task_id='process_backlogs',
     python_callable=run_backlog_processing,
     dag = dag)
+
 dummy_task = DummyOperator(
     task_id='dummy_task',
-    dag=dag
-)
-def run_min_to_hour():
-    os.system(f'spark-submit --conf spark.cores.max=4 ' +\
-    '$sparkf ~/eCommerce/data-processing/min_to_hour.py')
-
-min_to_hour = PythonOperator(
-  task_id='min_to_hour',
-  python_callable=run_min_to_hour,
-  dag = dag)
-# min_to_hour = PythonOperator(
-#   task_id='min_to_hour',
-#   python_callable=run_min_to_hour,
-#   dag = dag)
+    dag=dag)
 
 
 new_file_sensor >> check_backlog >>  dummy_task >> spark_live_process # >> min_to_hour
