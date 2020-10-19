af87cb78580b4a555072f7758a0bb908d9a7fc7a
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-19 04:15:09 -0400

    spark sql test

--- data-processing/test_spark_read_sql_query.py
+++ data-processing/test_spark_read_sql_query.py
@@ -19,7 +19,7 @@ def read_sql_to_df_time(spark,t0, t1, event='purchase', dim='product_id',suffix=
         #
     query = f"""
     (SELECT * FROM {event}_{dim}_{suffix} as dt
-    WHERE event_time BETWEEN '2019-10-01 00:10:00' and '2019-10-01 01:10:00'
+    WHERE event_time BETWEEN \'{t0}\' and \'{t1}\' 
     ORDER BY event_time
     ) as foo
     """
