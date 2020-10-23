2caf61d07cedeeb5c264506e81195c2a5c37ed8a
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2020-10-23 01:33:22 -0400

    more code cleaning

--- utility/mimic_server.py
+++ utility/mimic_server.py
@@ -1,35 +1,41 @@
+''' Move files from an AWS S3 directory into another directory to mimic server
+behavior, files are split into num_servers (determined by split_csv.py)
+'''
 import os
 import datetime as dt
-import sched, time
+import sched
+import time
 
 # s3 parameters
-bucket = 'maxwell-insight'
-src_dir = 'minicsv/'
-dst_dir = 'serverpool/'
+BUCKET = 'maxwell-insight'
+SRC_DIR = 'minicsv/'
+DST_DIR = 'serverpool/'
 
 # send file parameters
-num_servers = 4
-t_start = dt.datetime(2019,10,1,19,16,0)
-t_end = dt.datetime(2019,10,8,2,23,0)
-t_freq = dt.timedelta(minutes=1)
-t_gap = 60 #seconds
-t_format = "%Y-%m-%d-%H-%M-%S"
+NUM_SERVERS = 4
+T_START = dt.datetime(2019, 10, 1, 19, 16, 0)
+T_END = dt.datetime(2019, 10, 8, 2, 23, 0)
+T_GAP = 60 #seconds
+T_FORMAT = "%Y-%m-%d-%H-%M-%S"
 
 # scheduler
 s = sched.scheduler(time.time, time.sleep)
 
 # initialize time
-t_curr = t_start
+T_CURR = T_START
 
 def move_s3_file(sc, t_curr, t_start, t_end, t_gap):
-    t_str = t_curr.strftime(t_format)
-    print (t_str)
-    for i in range(num_servers):
-        os.system(f's3cmd cp s3://{bucket}/{src_dir}{t_str}-{i}.csv s3://{bucket}/{dst_dir}')
+    ''' Moves files between AWS S3 directories
+    '''
+    t_str = t_curr.strftime(T_FORMAT)
+    print(t_str)
+    for i in range(NUM_SERVERS):
+        os.system(f's3cmd cp s3://{BUCKET}/{SRC_DIR}{t_str}-{i}.csv s3://{BUCKET}/{DST_DIR}')
     t_curr += dt.timedelta(minutes=1)
-    if t_curr > t_end: t_curr = t_start
-    s.enter(t_gap, 1, move_s3_file, (sc,t_curr,t_start,t_end,t_gap,))
+    if t_curr > t_end:
+        t_curr = t_start
+    s.enter(t_gap, 1, move_s3_file, (sc, t_curr, t_start, t_end, t_gap,))
 
 if __name__ == "__main__":
-    s.enter(0, 1, move_s3_file, (s,t_curr,t_start,t_end,t_gap,))
+    s.enter(0, 1, move_s3_file, (sc, T_CURR, T_START, T_END, T_GAP,))
     s.run()
--- utility/split_csv.py
+++ utility/split_csv.py
@@ -1,50 +1,59 @@
-import pandas as pd
-import time, datetime, os
-import numpy as np
+''' Split large csv files of online events from a online retail store into
+smaller csv files. Also "compressing" the time, which is determined by t_step.
+For example, t_step=30 will be 30 times denser, ie. 30 days of data will be packed
+into a single day.
+'''
+import time
 from io import StringIO # python3; python2: BytesIO
+import numpy as np
+import pandas as pd
 import boto3
 
-def main(key = '2020-Feb'):
-    key = key + ".csv"
-    # key = 'sample.csv'
+def main(key='2020-Feb'):
+    '''Split large csv files of online events from a online retail store into
+    smaller csv files. Also "compressing" the time, which is determined by t_step.
+    For example, t_step=30 will be 30 times denser, ie. 30 days of data will be packed
+    into a single day.
+    '''
+    key += ".csv"
     ################################################################################
     # read csv file on s3 into spark dataframe
-    region = 'us-east-2'
     bucket = 'maxwell-insight'
-    s3file = "s3a://" + bucket + "/"+ key
-    print (s3file)
+    s3file = f"s3a://{bucket}/{key}"
+    print(s3file)
     # read csv file on s3 into spark dataframe
     df = pd.read_csv(s3file)
-    print (f'{time.asctime( time.localtime(time.time()) )}\n{key} read into pandas dataframe!')
+    print(f'{time.asctime( time.localtime(time.time()) )}\n{key} read into pandas dataframe!')
     ################################################################################
 
-    # compress time, 60 second -> 1 second
-    t_step = 30 # unit in seconds, timestamp will be grouped in steps with stepsize of t_step seconds
-    df['event_time'] = ((pd.to_datetime(df['event_time']) - pd.Timestamp("1970-01-01", tz='utc') )  / pd.Timedelta('1s'))
+    # compress time, 30 second -> 1 second if t_step=30
+    # unit in seconds, one second in the outputs will now contain t_step seconds
+    t_step = 30
+    df['event_time'] = ((pd.to_datetime(df['event_time']) -
+                         pd.Timestamp("1970-01-01", tz='utc')) / pd.Timedelta('1s'))
     t_min = pd.Timestamp("2019-10-01", tz='utc').value // 10**9
     df['event_time'] = df['event_time'] - t_min
     df['event_time'] = df['event_time'] // t_step
     df['event_time'] = (df['event_time'] + t_min).astype(np.int64)
-    df['event_time'] = pd.to_datetime(df['event_time'], unit='s')#.dt.strftime("%Y-%m-%d %H:%M:%S")
-    print (f'{time.asctime( time.localtime(time.time()) )}\nTime transformation complete\n')
+    df['event_time'] = pd.to_datetime(df['event_time'], unit='s')
+    print(f'{time.asctime( time.localtime(time.time()) )}\nTime transformation complete\n')
     t0 = df['event_time'].min()
     t1 = t0 + pd.Timedelta(seconds=60)
     t_end = df['event_time'].max()
 
     while t0 < t_end:
-        df_temp = df[ (df['event_time'] > t0) & (df['event_time'] < t1 )]
+        df_temp = df[(df['event_time'] > t0) & (df['event_time'] < t1)]
         for i in range(4):
             f_name = t0.strftime("%Y-%m-%d-%H-%M-%S") + '-' + str(i) + '.csv'
-            print (f_name)
+            print(f_name)
             df_i = df_temp.iloc[i::4, :]
             csv_buffer = StringIO()
             df_i.to_csv(csv_buffer)
             s3_resource = boto3.resource('s3')
             s3_resource.Object(bucket, "minicsv/" + f_name).put(Body=csv_buffer.getvalue())
         t0 += pd.Timedelta(seconds=60)
-        t1  = t0 + pd.Timedelta(seconds=60)
-
+        t1 = t0 + pd.Timedelta(seconds=60)
 
 if __name__ == "__main__":
-    for key in ["2019-Oct","2019-Nov","2020-Jan","2020-Feb","2020-Mar","2020-Apr"]:
-        main(key)
+    for k in ["2019-Oct", "2019-Nov", "2020-Jan", "2020-Feb", "2020-Mar", "2020-Apr"]:
+        main(k)
